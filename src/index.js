53def39a9061fbe43598bc2941614415670d3412
Seth Hollingsead <Iceversaka@hotmail.com>
2020-05-05 08:14:56 -0400

    Attempting to use ES6 syntax, first try

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+// import math from './math';
+import * as math from './math.js';
+
+// console.log(math.add(1,2));
+math.start();
 console.log('Hello Project.');
---
+++ src/math.js
@@ -0,0 +1,12 @@
+function add(x, y) {
+  return x + y;
+};
+
+function start() {
+  console.log('BEGIN math.start function');
+};
+
+export default {
+  add,
+  start
+};
