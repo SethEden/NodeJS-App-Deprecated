86ce19e924e088ea6e034a2675fd62290d173b1d
Seth Hollingsead <Iceversaka@hotmail.com>
2020-05-13 14:13:23 -0400

    Attempting to launch a python script from the NodeJS application command prompt.

--- src/commandParser.js
+++ src/commandParser.js
@@ -1,3 +1,7 @@
+const pySpawn = require('child_process').spawn;
+
+var pyProcess;
+
 function initCommandParser(dataInput) {
   console.log('BEGIN initCommandParser');
   console.log('dataInput is: ' + dataInput);
@@ -6,6 +10,16 @@ function initCommandParser(dataInput) {
     returnData = false;
   } else {
     console.log(dataInput);
+    if (dataInput.includes('.py') === true) {
+      // Here we will need to execute a python script.
+      pyProcess = pySpawn('python', ['../src/hello.py']);
+
+      process.stdout.on('data', data => {
+        console.log(data.toString());
+      });
+    } else {
+
+    }
     returnData = true;
   }
   console.log('returnData is: ' + returnData);
