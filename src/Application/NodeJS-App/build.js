45108edc2f1715a1b043a34712c2f3bc7859efca
Seth Hollingsead <Iceversaka@hotmail.com>
2020-07-30 18:15:08 -0500

    feat: Clean up the code after a fully successfull build-deploy-release integration with the command engine.

--- src/Application/NodeJS-App/build.js
+++ src/Application/NodeJS-App/build.js
@@ -80,15 +80,6 @@ function deployApplication() {
     }
     let deploymentResult = warden.getConfigurationSetting(s.cdeploymentCompleted);
     console.log('Deployment was completed: ' + deploymentResult);
-    // if (warden.getConfigurationSetting(s.cPassAllConstantsValidations) === true) {
-    //   // console.log('SUCCESS: Constants Validation PASSED!!');
-    //   // copyResult = warden.deployApplication(c.cSourceResourcesPath, c.cBinaryResourcesPath);
-    //   // // console.log('Deployment was completed: ' + copyResult);
-    //   // warden.consoleLog(baseFileName + b.cDot + functionName, 'Deployment was completed: ' + copyResult);
-    //   // warden.setConfigurationSetting('deploymentCompleted', copyResult);
-    // } else {
-    //   console.log('ERROR: Build failed because of a failure in the constants validation system. Please fix ASAP before attempting another build.');
-    // }
   } catch (err) {
     console.error(err);
     warden.setConfigurationSetting('deploymentCompleted', false);
@@ -119,13 +110,6 @@ function releaseApplication() {
     }
     let releaseResult = warden.getConfigurationSetting(s.creleaseCompleted);
     console.log('Release was completed: ' + releaseResult);
-    // if (warden.getConfigurationSetting(s.cPassAllConstantsValidations) === true) {
-    //   // console.log('SUCCESS: Constants Validation PASSED!!');
-    //   // releaseResult = warden.releaseApplication(c.cBinaryRootPath, c.cBinaryReleasePath);
-    //   // warden.consoleLog(baseFileName + b.cDot + functionName, 'releaseResult is: ' + releaseResult);
-    // } else {
-    //   console.log('ERROR: Release failed because of a failure in the constants validation system. Please fix ASAP before attempting another release.');
-    // }
   } catch (err) {
     console.error(err);
   }
